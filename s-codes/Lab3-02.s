f73f861e75d75eb67766b030713d8709753d0f52
Rahul <r-vb@users.noreply.github.com>
2023-05-22 18:17:59 +0530

    22-05-23

--- s-codes/Lab3-02.s
+++ s-codes/Lab3-02.s
@@ -8,9 +8,9 @@
             MOV     R0, #0         ;R0 is initialized to 0
             MOV     R5, #10        ;R5 is counter register
             MOV     R3, #1         ;Data register
-loop        ADD     R0, R0, R3     ;R0 = R0 + R3
-            INC     R3             ;R3 = R3 + 1
+LOOP        ADD     R0, R0, R3     ;R0 = R0 + R3
+            ADD     R3, R3, #1     ;R3 = R3 + 1
             SUB     R5, R5, #1     ;Decrement counter
             CMP     R5, #0         ;Checking counter value
-            BNE     loop           ;Repeat if not zero
+            BNE     LOOP           ;Repeat if not zero
             END                    ;End of File
