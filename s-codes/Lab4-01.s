7d77f5a592c6e7ef2bf1a6de7e7eeabf3f17e5b5
Rahul <r-vb@users.noreply.github.com>
2023-06-25 23:42:13 +0530

    05-06-23

--- s-codes/Lab4-01.s
+++ s-codes/Lab4-01.s
@@ -2,37 +2,26 @@
 ;Summation is stored in RESULT.
 ;Values in NUM1 and NUM2 are two 64-bit input numbers.
 
-        AREA Addition, CODE, READONLY
-        ENTRY
+            AREA Addition, CODE, READONLY
+            ENTRY
 
-        ; Declare constants
-        NUM1    EQU 0x4000         ; Memory location of first 64-bit number
-        NUM2    EQU 0x4010         ; Memory location of second 64-bit number
-        RESULT  EQU 0x4020         ; Memory location to store the result
+            LDR     R0, =VALUE1
+            LDR     R1, [R0]
+            LDR     R2, [R0,#4]
+            LDR     R0, =VALUE2
+            LDR     R3, [R0]
+            LDR     R4, [R0,#4]
+            ADDS    R6, R2, R4
+            ADC     R5, R1, R3
+            LDR     R0, =RESULT
+            STR     R5, [R0]        ;Stores Result
+            STRH    R6, [R0,#4]
+STOP        B       STOP
 
-        ; Reset vector
-        RESET
-            LDR SP, =0x7FFF         ; Set stack pointer
-            BL main                 ; Branch to main function
-            B RESET                 ; Loop back to RESET
+VALUE1      DCD     0x11111111, 0x33333333
+VALUE2      DCD     0x44444444, 0x77777777
 
-        ; Main program
-        main
-            LDR R0, =NUM1           ; Load address of first number
-            LDR R1, =NUM2           ; Load address of second number
+            AREA    ANSWER, DATA, READWRITE
+RESULT      DCD     0x0, 0x0
 
-            ; Perform addition
-            LDMIA R0!, {R2-R5}      ; Load first number into R2-R5 (64-bit)
-            LDMIA R1!, {R6-R9}      ; Load second number into R6-R9 (64-bit)
-            ADDS R2, R2, R6         ; Add lower 32 bits
-            ADCS R3, R3, R7         ; Add carry bit and higher 32 bits
-            ADCS R4, R4, R8         ; Add carry bit and higher 32 bits
-            ADCS R5, R5, R9         ; Add carry bit and higher 32 bits
-
-            ; Store the result
-            STMIA RESULT, {R2-R5}   ; Store the result in RESULT
-
-            ; End of program
-            MOV PC, LR              ; Return from main and end program
-
-        END
+            END
